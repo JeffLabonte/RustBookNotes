c076a61d0bb6716595d2c78a6994548e467e9852
Jeff Labonte <jeff.labonte12@gmail.com>
2018-08-27 22:43:15 -0400

    Add rand dependency

---
+++ guessing_game/target/debug/deps/guessing_game-919d499b40edaf63.d
@@ -0,0 +1,5 @@
+/home/jeff/Dropbox/Development/Projects/Rust_Book/guessing_game/target/debug/deps/guessing_game-919d499b40edaf63: src/main.rs
+
+/home/jeff/Dropbox/Development/Projects/Rust_Book/guessing_game/target/debug/deps/guessing_game-919d499b40edaf63.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/deps/rand-09957ba8fff16deb.d
@@ -0,0 +1,7 @@
+/home/jeff/Dropbox/Development/Projects/Rust_Book/guessing_game/target/debug/deps/librand-09957ba8fff16deb.rlib: /home/jeff/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/lib.rs /home/jeff/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/distributions/mod.rs /home/jeff/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/rand_impls.rs
+
+/home/jeff/Dropbox/Development/Projects/Rust_Book/guessing_game/target/debug/deps/rand-09957ba8fff16deb.d: /home/jeff/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/lib.rs /home/jeff/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/distributions/mod.rs /home/jeff/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/rand_impls.rs
+
+/home/jeff/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/lib.rs:
+/home/jeff/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/distributions/mod.rs:
+/home/jeff/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.22/src/rand_impls.rs:
