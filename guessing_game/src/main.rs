1292f4cb38ff1b59ec4eb5a96fd19f7b407871cb
Jeff Labonte <jeff.labonte12@gmail.com>
2018-08-29 21:37:36 -0400

    Add loop and break to game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,25 +7,30 @@ use rand::{ thread_rng, Rng };
 fn main() {
     println!("Guess the number !");
     let mut rng = thread_rng();
-    let secret_number = rng.gen_range(0,101);
+    let secret_number = rng.gen_range(0, 101);
 
     println!("The secret number is {}", secret_number);
+    loop {
+        println!("Please input your guess");
 
-    println!("Please input your guess");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess:u32 = guess.trim().parse()
+        let guess: u32 = guess.trim().parse()
             .expect("Please type a number");
 
-    println!("You guessed {}", guess);
+        println!("You guessed {}", guess);
 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
+
