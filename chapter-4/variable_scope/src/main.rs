1aff0da4d68cf446f9199c11e54f018ac3ae71fb
Jeff Labonte <jeff.labonte12@gmail.com>
2018-09-02 17:41:41 -0400

    Create example for variable scope

--- chapter-4/variable_scope/src/main.rs
+++ chapter-4/variable_scope/src/main.rs
@@ -1,3 +1,4 @@
-fn main() {
-    println!("Hello, world!");
-}
+fn main() {// S is not valid here! It's not yet declared
+    let s = "Hello"; // S is valid from this point forward
+    // do stuff with s
+}// This scipe is now over, and is not longer valid
