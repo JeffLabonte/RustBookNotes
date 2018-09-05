1457da8aa24f00f8d2d39c31f8ee66f858a6e082
Jeff Labonte <jeff.labonte12@gmail.com>
2018-09-04 20:40:35 -0400

    Add dangling references that crashes

---
+++ chapter-4/dangling_references/target/debug/deps/danglin_references-b24af72099700b2e.d
@@ -0,0 +1,5 @@
+/home/jeff/Dropbox/Development/Projects/Rust_Book/chapter-4/dangling_references/target/debug/deps/danglin_references-b24af72099700b2e: src/main.rs
+
+/home/jeff/Dropbox/Development/Projects/Rust_Book/chapter-4/dangling_references/target/debug/deps/danglin_references-b24af72099700b2e.d: src/main.rs
+
+src/main.rs:
