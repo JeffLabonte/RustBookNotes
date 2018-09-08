85a6dd6882fbb8eed7c72d4702a27adc99bc2c33
Jeff Labonte <jeff.labonte12@gmail.com>
2018-09-08 14:57:22 -0400

    Create method for rectangle

--- chapter-5/rectangle/src/main.rs
+++ chapter-5/rectangle/src/main.rs
@@ -1,6 +1,13 @@
-struct Rectangle{
+#[derive(Debug)] // To make the println! in rectangle_rect_way work (last print)
+struct Rectangle{ // This is a traits derived to our struct
     width: u32,
     height: u32
+}
+
+impl Rectangle{
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
 fn main() {
@@ -16,7 +23,7 @@ fn rectangle_variable_way(){
     println!(
         "The area of the rectangle is {} square pixels",
         area(width, height)
-    )
+    );
 }
 
 fn area(width: u32, height: u32) -> u32{
@@ -28,7 +35,7 @@ fn rectangle_tuple_way(){
     println!(
         "The area of the rectangle is {} square pixels",
         area_tuple(rect1)
-    )
+    );
 }
 
 fn area_tuple(dimensions: (u32, u32)) -> u32{
@@ -45,7 +52,11 @@ fn rectangle_rect_way(){
     println!(
         "The area of the rectangle is {} square pixels",
         area_struct(&rect1) // We borrow to keep ownership here!
-    )
+    );
+
+    println!("This is our rectangle: {:?}", rect1);
+
+    println!("Calling Rectangle method area {}", rect1.area());
 }
 
 fn area_struct(rect: &Rectangle) -> u32{ // &Rectangle is borrowed ( Referenced )
