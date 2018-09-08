77664a369c5a3d49e017ecca56e66ec8b40cb316
Jeff Labonte <jeff.labonte12@gmail.com>
2018-09-08 14:37:57 -0400

    Same example with tuples

--- chapter-5/rectangle/src/main.rs
+++ chapter-5/rectangle/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     rectangle_variable_way();
+    rectangle_tuple_way();
 }
 
 fn rectangle_variable_way(){
@@ -16,4 +17,14 @@ fn area(width: u32, height: u32) -> u32{
     width * height
 }
 
+fn rectangle_tuple_way(){
+    let rect1 = (30, 50);
+    println!(
+        "The area of the rectangle is {} square pixels",
+        area_tuple(rect1)
+    )
+}
 
+fn area_tuple(dimensions: (u32, u32)) -> u32{
+    dimensions.0 * dimensions.1
+}
