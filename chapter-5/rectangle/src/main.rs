c798e6ca0345c3ee92523326ef9dddccaba06fe1
Jeff Labonte <jeff.labonte12@gmail.com>
2018-09-10 22:27:35 -0400

    Add example with enum

--- chapter-5/rectangle/src/main.rs
+++ chapter-5/rectangle/src/main.rs
@@ -8,21 +8,12 @@ impl Rectangle{
     fn area(&self) -> u32 {
         self.width * self.height
     }
-
-    fn can_hold(&self, other:& Rectangle) -> bool{
-        self.width > other.width && self.height > other.height
-    }
-
-    fn square(size: u32) -> Rectangle { // Associated function
-        Rectangle{ width: size, height: size }
-    }
 }
 
 fn main() {
     rectangle_variable_way();
     rectangle_tuple_way();
     rectangle_rect_way();
-    create_square(); //Associate functions used
 }
 
 fn rectangle_variable_way(){
@@ -57,11 +48,6 @@ fn rectangle_rect_way(){
         height: 50
     };
 
-    let rect2 = Rectangle{
-        width: 40,
-        height: 50
-    };
-
 
     println!(
         "The area of the rectangle is {} square pixels",
@@ -71,18 +57,8 @@ fn rectangle_rect_way(){
     println!("This is our rectangle: {:?}", rect1);
 
     println!("Calling Rectangle method area {}", rect1.area());
-
-    println!("Rect1 can hold rect2? {}", rect1.can_hold(&rect2));
 }
 
 fn area_struct(rect: &Rectangle) -> u32{ // &Rectangle is borrowed ( Referenced )
     rect.width * rect.height 
-}
-
-fn create_square(){
-    let sq = Rectangle::square(15); // :: is used for namspace and to call associated function
-
-    println!("This is our square {}", sq.area());
-
-    println!("This is the object {:?}", sq);
 }
