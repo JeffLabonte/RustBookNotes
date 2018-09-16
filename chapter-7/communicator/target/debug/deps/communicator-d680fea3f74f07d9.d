f1e11246b64149f524c1129a0fbf649971c3d51b
Jeff Labonte <jeff.labonte12@gmail.com>
2018-09-16 14:39:33 -0400

    Remove the app project

---
+++ chapter-7/communicator/target/debug/deps/communicator-d680fea3f74f07d9.d
@@ -0,0 +1,5 @@
+/home/jeff/Dropbox/Development/Projects/Rust_Book/chapter-7/communicator/target/debug/deps/communicator-d680fea3f74f07d9: src/main.rs
+
+/home/jeff/Dropbox/Development/Projects/Rust_Book/chapter-7/communicator/target/debug/deps/communicator-d680fea3f74f07d9.d: src/main.rs
+
+src/main.rs:
