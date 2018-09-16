51769835df892c80df9eea42626f49d11294fa09
Jeff Labonte <jeff.labonte12@gmail.com>
2018-09-16 16:17:09 -0400

    Change test to use modules

--- chapter-7/communicator/src/lib.rs
+++ chapter-7/communicator/src/lib.rs
@@ -1,11 +1,12 @@
+pub mod client;
+
+pub mod network;
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        super::client::connect();
     }
 }
 
-pub mod client;
-
-pub mod network;
---
+++ chapter-7/communicator/target/debug/deps/communicator-988afc0cb2995f82.d
@@ -0,0 +1,5 @@
+/home/jeff/Dropbox/Development/Projects/Rust_Book/chapter-7/communicator/target/debug/deps/communicator-988afc0cb2995f82: src/main.rs
+
+/home/jeff/Dropbox/Development/Projects/Rust_Book/chapter-7/communicator/target/debug/deps/communicator-988afc0cb2995f82.d: src/main.rs
+
+src/main.rs:
