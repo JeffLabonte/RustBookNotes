6ad2419c0e19bb64bbd8134dbaaf51f98bb2f7f8
Jeff Labonte <jeff.labonte12@gmail.com>
2018-09-17 21:37:24 -0400

    Add example on ownership with hashmaps

--- chapter-8/hashmap_example/src/main.rs
+++ chapter-8/hashmap_example/src/main.rs
@@ -10,5 +10,14 @@ fn main() {
 
     let initial_scores = vec![10, 50];
 
-    let scores_second:HashMap<_,_> = teams.iter().zip(initial_scores.iter()).collect();
+    let scores_second:HashMap<_,_> = teams.iter().zip(initial_scores.iter()).collect(); // <_,_> is required here since rust
+    // has no idea what data structure you might want to bring in!
+    
+    let field_name = String::from("My favorite Color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+
+    map.insert(field_name, field_value); // At this point the two strings belongs to the hashmap
+    // You can't reuse the variables field_name and field_value
 }
