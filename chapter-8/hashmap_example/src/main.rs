587d1138204ebf766bdf11640c3d269ceaa10fff
Jeff Labonte <jeff.labonte12@gmail.com>
2018-09-17 21:33:17 -0400

    Second way to create hashmap
    This one uses vectors and other methods

--- chapter-8/hashmap_example/src/main.rs
+++ chapter-8/hashmap_example/src/main.rs
@@ -4,5 +4,11 @@ fn main() {
     let mut scores = HashMap::new();
 
     scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yello"), 50);
+    scores.insert(String::from("Yellow"), 50);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+
+    let initial_scores = vec![10, 50];
+
+    let scores_second:HashMap<_,_> = teams.iter().zip(initial_scores.iter()).collect();
 }
