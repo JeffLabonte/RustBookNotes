cd4ba1ebab322101c13707f41ccc006dd2fd9b0d
Jeff Labonte <jeff.labonte12@gmail.com>
2018-09-18 20:20:30 -0400

    Add example to code - Error handling reading file

---
+++ chapter-9/error_handling/target/debug/deps/error_handling-63cfb796c83d2271.d
@@ -0,0 +1,5 @@
+/home/jeff/Dropbox/Development/Projects/Rust_Book/chapter-9/error_handling/target/debug/deps/error_handling-63cfb796c83d2271: src/main.rs
+
+/home/jeff/Dropbox/Development/Projects/Rust_Book/chapter-9/error_handling/target/debug/deps/error_handling-63cfb796c83d2271.d: src/main.rs
+
+src/main.rs:
---
+++ chapter-9/error_handling/target/debug/error_handling.d
@@ -0,0 +1,1 @@
+/home/jeff/Dropbox/Development/Projects/Rust_Book/chapter-9/error_handling/target/debug/error_handling: /home/jeff/Dropbox/Development/Projects/Rust_Book/chapter-9/error_handling/src/main.rs
