da15ef43b3621c0926b69a4eccebc9b01d8058e3
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-03 20:27:33 -0400

    Add the part that reads a file

---
+++ The_Rust_Programming_Language_Notes/chapter-12/minigrep/target/rls/debug/deps/minigrep-57b5ddb5a42f2fa1.d
@@ -0,0 +1,5 @@
+/home/jeff/Dropbox/Development/Projects/Rust_Book/The_Rust_Programming_Language_Notes/chapter-12/minigrep/target/rls/debug/deps/minigrep-57b5ddb5a42f2fa1.rmeta: src/main.rs
+
+/home/jeff/Dropbox/Development/Projects/Rust_Book/The_Rust_Programming_Language_Notes/chapter-12/minigrep/target/rls/debug/deps/minigrep-57b5ddb5a42f2fa1.d: src/main.rs
+
+src/main.rs:
