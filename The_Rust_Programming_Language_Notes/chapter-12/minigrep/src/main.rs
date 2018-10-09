7c64d3c91de0614a7dbaef92b40cd374cd8473cd
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-09 19:46:01 -0400

    Remove function for a impl struct

--- The_Rust_Programming_Language_Notes/chapter-12/minigrep/src/main.rs
+++ The_Rust_Programming_Language_Notes/chapter-12/minigrep/src/main.rs
@@ -7,22 +7,24 @@ struct Config {
     filename: String,
 }
 
+impl Config{
+    fn new(args: &[String]) -> Config{
+        let query = args[1].clone(); // TODO Change for a more efficient
+        let filename = args[2].clone(); // TODO way
+
+        Config{query, filename}
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let configs = parse_config(&args);
-    println!("Searching for {}", configs.query);
-    println!("Filename : {}", configs.filename);
+    let config = Config::new(&args);
+    println!("Searching for {}", config.query);
+    println!("Filename : {}", config.filename);
 
-    let content = fs::read_to_string(configs.filename)
+    let content = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With text: {}\n", content);
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone(); // TODO Change for a more efficient
-    let filename = args[2].clone(); // TODO way
-
-    Config{query, filename}
 }
