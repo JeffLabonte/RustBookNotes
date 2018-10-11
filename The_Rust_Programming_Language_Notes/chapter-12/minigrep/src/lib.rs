cb0930dee1d76b2001769245f024a7aaf80ebcb0
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-10 21:48:36 -0400

    Make the test work

--- The_Rust_Programming_Language_Notes/chapter-12/minigrep/src/lib.rs
+++ The_Rust_Programming_Language_Notes/chapter-12/minigrep/src/lib.rs
@@ -28,12 +28,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{ // NOTE dyn (dynamic) 
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut result = Vec::new();
     for line in contents.lines() {
         if line.contains(query){
-            println!("{}", line);
+            result.push(line);
         }
     }
-    vec![]
+    result
 }
 
 #[cfg(test)]
