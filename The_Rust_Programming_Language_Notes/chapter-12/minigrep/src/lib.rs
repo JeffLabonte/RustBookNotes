5be7588664837db3ee6024c8dc2f9f42a6add80c
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-10 21:37:21 -0400

    Add iteration of lines

--- The_Rust_Programming_Language_Notes/chapter-12/minigrep/src/lib.rs
+++ The_Rust_Programming_Language_Notes/chapter-12/minigrep/src/lib.rs
@@ -28,6 +28,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{ // NOTE dyn (dynamic) 
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    for line in contents.lines() {
+        println!("{}", line);
+    }
     vec![]
 }
 
