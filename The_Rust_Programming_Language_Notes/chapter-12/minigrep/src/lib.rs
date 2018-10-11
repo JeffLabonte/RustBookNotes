d984f1831b2fc48be623145edafee9f4b52e8118
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-11 00:01:42 -0400

    Add test without the functions implemented

--- The_Rust_Programming_Language_Notes/chapter-12/minigrep/src/lib.rs
+++ The_Rust_Programming_Language_Notes/chapter-12/minigrep/src/lib.rs
@@ -56,4 +56,19 @@ Pick three.";
             search(query, contents)
         );
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query - "rUst";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        
+        assert_eq!(
+            vec!["Rust:", "Trust me"],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
+
