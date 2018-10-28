0141e211ef9176b0e328697fbe5637d8551a83b5
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-27 23:12:40 -0400

    Use generics to implement 'better' a trait

--- The_Rust_Programming_Language_Notes/chapter-17/trait-oop/src/lib.rs
+++ The_Rust_Programming_Language_Notes/chapter-17/trait-oop/src/lib.rs
@@ -2,14 +2,28 @@ pub trait Draw{
     pub draw(&self);
 }
 
-pub struct Screen{
-    pub components: Vec<Box<dyn Draw>>,
+/// First iteration of code
+//pub struct Screen{
+//    pub components: Vec<Box<dyn Draw>>,
+//}
+//
+//impl Screen{
+//    pub fn run(&self){
+//        for component in self.components.iter(){
+//            component.draw();
+//        }
+//    }
+//}
+
+pub struct Screen<T: Draw>{
+    pub components: Vec<T>.
 }
 
-impl Screen{
-    pub fn run(&self){
-        for component in self.components.iter(){
-            component.draw();
+impl<T> Screen <T>
+    where T: Draw{
+        pub fn run(&self){
+            for component in self.components.iter(){
+                component.draw();
+            }
         }
-    }
 }
