a7d3807a9ebba8d80ff09692dc074e45ebb2430e
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-28 15:10:30 -0400

    Using the encoding pattern instead of the state pattern

--- The_Rust_Programming_Language_Notes/chapter-17/trait-oop/blog/src/lib.rs
+++ The_Rust_Programming_Language_Notes/chapter-17/trait-oop/blog/src/lib.rs
@@ -90,6 +90,10 @@ pub struct DraftPost {
     content: String,
 }
 
+pub struct PendingReviewPost{
+    content: String,
+}
+
 impl Post {
     pub fn new() -> DraftPost{
         DraftPost{
@@ -105,5 +109,19 @@ impl Post {
 impl DraftPost{
     pub fn add_text(&mut self, text: &str) {
         self.content.push_str(text);
+    }
+
+    pub fn request_review(self) -> PendingReviewPost{
+        PendingReviewPost{
+            content: self.content
+        }
+    }
+}
+
+impl PendingReviewPost{
+    pub fn approve(self) -> Post{
+        Post{
+            content: self.content
+        }
     }
 }
