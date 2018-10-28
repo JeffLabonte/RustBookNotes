54880dc01fff91cf7330f7052a54d65895fd5453
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-28 16:58:54 -0400

    Add lifetime to all parts of code so no one outlive anyone

---
+++ The_Rust_Programming_Language_Notes/chapter-19/advanced_lifetime/target/debug/deps/advanced_lifetime-1c7a69908f1d3f72.d
@@ -0,0 +1,5 @@
+/home/jeff/Sync/Development/Projects/Rust_Book/The_Rust_Programming_Language_Notes/chapter-19/advanced_lifetime/target/debug/deps/libadvanced_lifetime-1c7a69908f1d3f72.rlib: src/lib.rs
+
+/home/jeff/Sync/Development/Projects/Rust_Book/The_Rust_Programming_Language_Notes/chapter-19/advanced_lifetime/target/debug/deps/advanced_lifetime-1c7a69908f1d3f72.d: src/lib.rs
+
+src/lib.rs:
---
+++ The_Rust_Programming_Language_Notes/chapter-19/advanced_lifetime/target/debug/deps/advanced_lifetime-42084331c2f8c0a7.d
@@ -0,0 +1,5 @@
+/home/jeff/Sync/Development/Projects/Rust_Book/The_Rust_Programming_Language_Notes/chapter-19/advanced_lifetime/target/debug/deps/advanced_lifetime-42084331c2f8c0a7: src/main.rs
+
+/home/jeff/Sync/Development/Projects/Rust_Book/The_Rust_Programming_Language_Notes/chapter-19/advanced_lifetime/target/debug/deps/advanced_lifetime-42084331c2f8c0a7.d: src/main.rs
+
+src/main.rs:
