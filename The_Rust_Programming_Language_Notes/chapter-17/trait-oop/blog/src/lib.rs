6962f9bab349605d80af76696c4ccb225c87ced4
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-28 11:47:24 -0400

    Add missing parts of the example

--- The_Rust_Programming_Language_Notes/chapter-17/trait-oop/blog/src/lib.rs
+++ The_Rust_Programming_Language_Notes/chapter-17/trait-oop/blog/src/lib.rs
@@ -26,8 +26,22 @@ impl Post {
     }
 }
 
-trait State {}
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+}
 
 struct Draft {}
 
-impl State for Draft {}
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State>{
+        Box::new(PendingReview {})
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview{
+    fn request_review(self: Box<Self>) -> Box<dyn State>{
+        self
+    }
+}
