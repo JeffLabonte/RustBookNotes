01c58b7d786853e68189c40bf1c9d9349efbe62e
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-28 23:05:13 -0400

    Mimic sleep time

--- The_Rust_Programming_Language_Notes/chapter-20/hello/src/main.rs
+++ The_Rust_Programming_Language_Notes/chapter-20/hello/src/main.rs
@@ -1,12 +1,15 @@
 use std::io::prelude::*;
 use std::net::{ TcpListener, TcpStream};
 use std::fs;
+use std::thread;
+use std::time::Duration;
 
 static HTTP_OK_STATUS:&str = "HTTP/1.1 200 OK\r\n\r\n";
 static HTTP_NOT_FOUND_STATUS:&str = "HTTP/1.1 404 NOT FOUND \r\n\r\n";
 
 static HTTP_GET_METHOD:&[u8]  = b"GET / HTTP/1.1\r\n";
 static HTTP_POST_METHOD:&[u8]  = b"POST / HTTP/1.1\r\n";
+static HTTP_SLEEP_METHOD:&[u8] = b"GET /sleep HTTP/1.1\r\n";
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -22,7 +25,11 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
     let (status_line, filename) = if buffer.starts_with(HTTP_GET_METHOD){
         (HTTP_OK_STATUS, "hello.html")
-    }else{
+    } else if buffer.starts_with(HTTP_SLEEP_METHOD){
+        thread::sleep(Duration::from_secs(5));
+        (HTTP_OK_STATUS, "hello.html")
+    }
+    else{
         (HTTP_NOT_FOUND_STATUS, "404.html")
     };
 
