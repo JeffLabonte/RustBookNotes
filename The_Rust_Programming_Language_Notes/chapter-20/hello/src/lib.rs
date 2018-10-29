2b555ef9bec7677e0d1efde7729a27550d305fec
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-29 19:41:26 -0400

    Add some docs

--- The_Rust_Programming_Language_Notes/chapter-20/hello/src/lib.rs
+++ The_Rust_Programming_Language_Notes/chapter-20/hello/src/lib.rs
@@ -1,7 +1,20 @@
 pub struct ThreadPool;
 
 impl ThreadPool{
+    /// Create a new ThreadPool
+    ///
+    /// The size is the number of threads in the pool
+    ///
+    /// # Panics
+    ///
+    /// The 'new' will panic if size  is 0
     pub fn new(size: usize) -> ThreadPool{
+        assert(size > 0);
         ThreadPool
     }
+
+    pub fn execute<F>(&self, f: F)
+        where F: FnOnce() + Send + 'static{
+            
+        }
 }
