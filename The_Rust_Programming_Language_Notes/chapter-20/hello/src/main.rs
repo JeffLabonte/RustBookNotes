2657830c2aec3c6b10f8abb048a8a9c5f2822dd2
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-28 22:07:25 -0400

    Add possibility to handle POST and others as well

--- The_Rust_Programming_Language_Notes/chapter-20/hello/src/main.rs
+++ The_Rust_Programming_Language_Notes/chapter-20/hello/src/main.rs
@@ -2,6 +2,8 @@ use std::io::prelude::*;
 use std::net::{ TcpListener, TcpStream};
 use std::fs;
 
+static GET_METHOD:&[u8]  = b"GET / HTTP/1.1\r\n";
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -14,9 +16,14 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    let content = fs::read_to_string("hello.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", content);
+    if buffer.starts_with(GET_METHOD) {
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap(); // INFO: Close the stream 
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap(); // INFO: Close the stream 
+    }else{
+        //TODO Handle different calls
+    }
+
 }
