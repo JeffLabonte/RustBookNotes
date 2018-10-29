e81cb04b821426b54c51402b93c21701cc1a9b86
Jeff Labonte <jeff.labonte12@gmail.com>
2018-10-29 19:58:23 -0400

    Change new to create part of the ThreadPool

--- The_Rust_Programming_Language_Notes/chapter-20/hello/src/lib.rs
+++ The_Rust_Programming_Language_Notes/chapter-20/hello/src/lib.rs
@@ -1,4 +1,8 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool{
+    threads: Vec<thread::JoinHandle<()>>
+};
 
 impl ThreadPool{
     /// Create a new ThreadPool
@@ -8,9 +12,17 @@ impl ThreadPool{
     /// # Panics
     ///
     /// The 'new' will panic if size  is 0
-    pub fn new(size: usize) -> ThreadPool{
+    pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError>{
         assert(size > 0);
-        ThreadPool
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size{
+            threads.push(thread::Builder::new());
+        }
+
+        ThreadPool{
+            threads
+        }
     }
 
     pub fn execute<F>(&self, f: F)
